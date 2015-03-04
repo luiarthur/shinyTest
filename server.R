d1a89981a1981e89df64422b4679025e5da1b6a0
luiarthur <luiarthur@yahoo.com>
2015-03-03 17:43:17 -0700

    yes

--- server.R
+++ server.R
@@ -44,15 +44,20 @@ shinyServer(function(input,output) {
 
   if (submit) {
     output$matFreq = renderDataTable({
-      (result())$M$info
+      result$M$info
+      #(result())$M$info
     })
 
     output$expVal = renderPlot({
      par(mfrow=c(3,1))
-      a.image(result()$EZO,number=T,main=paste("E[IBP], E[ncol] =",result()$mncolo))
-      a.image(result()$EZA,number=T,main=paste("E[AIBP], E[ncol] =",result()$mncola))
-      a.image(result()$EZD,number=T, main=paste("E[ddIBP], E[ncol] =",
-              result()$mncold))
+      a.image(result$EZO,number=T,main=paste("E[IBP], E[ncol] =",result$mncolo))
+      a.image(result$EZA,number=T,main=paste("E[AIBP], E[ncol] =",result$mncola))
+      a.image(result$EZD,number=T, main=paste("E[ddIBP], E[ncol] =",
+              result$mncold))
+      #a.image(result()$EZO,number=T,main=paste("E[IBP], E[ncol] =",result()$mncolo))
+      #a.image(result()$EZA,number=T,main=paste("E[AIBP], E[ncol] =",result()$mncola))
+      #a.image(result()$EZD,number=T, main=paste("E[ddIBP], E[ncol] =",
+      #        result()$mncold))
       par(mfrow=c(1,1))
     })
   }
