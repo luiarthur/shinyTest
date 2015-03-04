44a82ac821a8884968b3e8dcf67db6f1fbeebd2c
luiarthur <luiarthur@yahoo.com>
2015-03-03 17:33:20 -0700

    yes

--- server.R
+++ server.R
@@ -25,9 +25,8 @@ shinyServer(function(input,output) {
   isolate(input$distMatNum)
   isolate(input$alpha)
   isolate(input$its)
-
-  result <- one.sim(paste0("D",input$distMatNum), a=input$alpha,B=input$its)
-
+  result <- reactive({one.sim(paste0("D",input$distMatNum),
+                      a=input$alpha,B=input$its)})
 
 
   output$distMat = renderUI({
